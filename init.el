843b05ed811739f059346e6fb3c80770f9ab7f33
Robert Krause <robert.allen.krause@gmail.com>
2014-04-22 14:19:36 -0500

    - Fix rainbow-delimeters to use elpa appropriately.

--- init.el
+++ init.el
@@ -59,14 +59,14 @@
 (require 'rcirc)
 (rcirc-track-minor-mode 1)
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((Base . 10) (Syntax . ANSI-Common-Lisp)))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(rainbow-delimiters-depth-4-face ((t (:foreground "#970098")))))
