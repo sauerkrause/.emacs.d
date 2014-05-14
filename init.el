e221b47491845f73579024c5701e40e474e8ba1e
Robert Krause <robert.allen.krause@gmail.com>
2014-05-14 10:46:22 -0500

    - Added package archives for melpa and marmalade

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 (add-to-list 'load-path "~/.emacs.d/load-path/")
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize))
+
 (require 'android-common)
 (require 'android-compile)
 (require 'android-host)
