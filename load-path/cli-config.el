552f3f5e28abfb18c8b4008943252f49bb3f08d5
Robert Krause <robert.allen.krause@gmail.com>
2021-06-04 11:11:11 -0500

    changed cli theme
    I have converted to light mode in terminal thanks to blue
    light filtering eyeglasses

--- load-path/cli-config.el
+++ load-path/cli-config.el
@@ -2,6 +2,11 @@
 (defun cli-config-load ()
   "load my cli configuration"
   (unless (display-graphic-p)
-    (load-theme 'misterioso)))
+    (require 'all-the-icons)
+    (require 'doom-modeline)
+    (require 'doom-themes)
+    (setq doom-themes-enable-bold t
+	  doom-themes-enable-italic t)
+    (load-theme 'doom-opera-light t)))
 
 (provide 'cli-config)
